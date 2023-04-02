8146673eb259d85af2eca96fcb15778a759a3422
Idham Adi Leksono <idhamadileksono@gmail.com>
2023-04-02 13:34:53 +0700

    update

--- 04_Latihan/Main.py
+++ 04_Latihan/Main.py
@@ -4,3 +4,20 @@ class Hero:
     self.name = name
     self.health = health
     self.attackPower = attackPower
+    self.armorNumber = armorNumber
+    
+  def serang(self, lawan):
+    print(self.name + " menyerang " + lawan.name)
+    lawan.diserang(self, self.attackPower)
+  
+  def diserang(self, lawan, attackPower_lawan):
+    print(self.name + " diserang " + lawan.name)
+    attack_diterima = attackPower_lawan / self.armorNumber
+    print("Seranagan terasa: " + str(attack_diterima))
+    self.health -= attack_diterima
+    print("darah" + self.name + " tersisa " + str(attack_diterima))
+    
+sniper = Hero("Sniper", 100, 10, 5)
+rikimaru = Hero("rikimaru", 100, 5, 10)
+
+sniper.serang(rikimaru)
